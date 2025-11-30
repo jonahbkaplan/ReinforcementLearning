61669ecf59ec91ec03e667e7f5eeec47d220ecba
Smee18 <marcel.venturotti@icloud.com>
2025-11-30 13:48:37 +0000

    Added some comments for SAC

--- algorithms/SAC/Networks.py
+++ algorithms/SAC/Networks.py
@@ -2,20 +2,34 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Actor(nn.Module):
+class Actor(nn.Module): 
+    """
+    Actor network which ouputs means and standard deviations for action probability distributions
+    """
     def __init__(self, state_size):
+        """
+        Params:
+            state_size (int) -> number of frames we are using per iteration (default 4)
+        """
         super(Actor, self).__init__()
 
+        # Downscale image through convolution
         self.conv1 = nn.Conv2d(in_channels=state_size, out_channels=32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
 
+        #Linear condense
         self.fc = nn.Linear(3072, 512)
 
-        self.mu_layer = nn.Linear(512, 2)
-        self.log_std_layer = nn.Linear(512, 2)
+        self.mu_layer = nn.Linear(512, 2) # Means for acceleration and steering
+        self.log_std_layer = nn.Linear(512, 2) # Log variance for acceleration and steering
 
     def forward(self, state, fusion=False):
+        """
+        Params:
+            state  (array of size [4, 128, 64]) -> the set of grayscale images to be fed into the CNN
+            fusion (bool) -> wheter we return the flattened output or the distribution params
+        """
         # x shape: (Batch, 4, 128, 64)
         
         x = state / 255.0
@@ -32,7 +46,7 @@ class Actor(nn.Module):
         x = F.relu(self.fc(x))
         
 
-        if fusion:
+        if fusion: 
             # Get Distribution Parameters
             mu = self.mu_layer(x)
             log_std = self.log_std_layer(x)
@@ -46,7 +60,15 @@ class Actor(nn.Module):
             return x
 
 class Critic(nn.Module):
+    """
+    Actor network which ouputs value function from given state and action sampled from actor
+    """
     def __init__(self, state_dim, action_dim):
+        """
+        Params:
+            state      (array of size [512]) -> the set of grayscale images to be fed into the CNN
+            action_dim (array of size [2]) -> acceleration and sterring values
+        """
         super(Critic, self).__init__()
         # Critic takes State + Action as input
         self.l1 = nn.Linear(state_dim + action_dim, 256)
@@ -54,6 +76,11 @@ class Critic(nn.Module):
         self.q_value = nn.Linear(256, 1)
 
     def forward(self, state, action):
+        """
+        Params:
+            state      (array of size [512]) -> the set of grayscale images to be fed into the CNN
+            action_dim (array of size [2]) -> acceleration and sterring values
+        """
         x = torch.cat([state, action], dim=1)
         x = F.relu(self.l1(x))
         x = F.relu(self.l2(x))
--- algorithms/SAC/SAC.py
+++ algorithms/SAC/SAC.py
@@ -8,13 +8,26 @@ import torch
 class SAC(Agent):
     # Soft Actor-Critic
     # Marcel  
-    def __init__(self, env):
+    def __init__(self, env, episodes = 100, discount = 0.9, alpha = 1.0, state_size = 4):
+        """
+        SAC Implemention.
+
+        Params:
+            env        (Object) -> the environment to act in
+            episodes   (int) -> of episodes to run the training algorithm
+            discount   (float) -> Discount factor (Gamma)
+            alpha      (float) -> Controls log probabilites
+            state_size (int) -> How many images passed into actor model
+        """
         super().__init__(env)
+
         self.env = env
         self.replayBuffer = []
         self.policyParams = 0
+        self.state_size = state_size
 
-        self.actor = Actor(4)
+        #Models
+        self.actor = Actor(self.state_size) # We are going for 4 images per iteration
 
         self.critic_1 = Critic()
         self.critic_2 = Critic()
@@ -22,16 +35,17 @@ class SAC(Agent):
         self.target_critic_1 = Critic()
         self.target_critic_2 = Critic()        
 
-        self.discount = 0.9
-        self.alpha = 1
+        # Hyperparameters
+        self.discount = discount
+        self.alpha = alpha
 
 
     def predict(self, obs):
         pass
 
     def learn(self) :
-        batch = random.choice(self.replayBuffer)
-        (state, action, reward, new_state, done) = batch
+        batch = random.choice(self.replayBuffer)         # Choose a random experience from the replay buffer
+        (state, action, reward, new_state, done) = batch # Unpack batch
 
         action_new = self.choose_action(new_state)
         Q1_action_value_new = self.target_critic_1(new_state, action_new)
@@ -47,8 +61,8 @@ class SAC(Agent):
     def collect_data(self):
 
         obs, _ = self.env.reset()
-        self.isDone = False
-        while not self.isDone:
+        self.isDone = False 
+        while not self.isDone: # Check is terminal
 
             action = self.choose_action(obs)
 
@@ -61,12 +75,12 @@ class SAC(Agent):
 
     
     def choose_action(self, obs):
-        state_tensor = torch.FloatTensor(obs).unsqueeze(0).to(self.device)
-        mu, log_std = self.actor(state_tensor)
+        state_tensor = torch.FloatTensor(obs).unsqueeze(0).to(self.device) # Convert to tensor for efficiency
+        mu, log_std = self.actor(state_tensor) # Actor model outputs 
         std = torch.exp(log_std)
-        dist = torch.distributions.Normal(mu, std)
+        dist = torch.distributions.Normal(mu, std) # 2D normal distrubution
         action_tensor = dist.sample()
-        action_tensor = torch.tanh(action_tensor)
-        action_numpy = action_tensor.cpu().numpy()[0]
+        action_tensor = torch.tanh(action_tensor) # Rescale between -1 and 1 to match environment logic
+        action_numpy = action_tensor.cpu().numpy()[0] # Back to numpy
 
         return action_numpy
