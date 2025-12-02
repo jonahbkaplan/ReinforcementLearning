415fd3c017d7c607e2af77f6b9dd3d3d54aefe96
rj-carey <carey.ruaidhri@gmail.com>
2025-12-02 14:43:23 +0000

    Moved Comments.

--- algorithms/REINFORCE.py
+++ algorithms/REINFORCE.py
@@ -19,64 +19,63 @@ class Reinforce(Agent):
         :param Real step_size_w: Step size for baseline function updates (Alpha_w)
         :param Array flags: index 0 indicates true or false to include baseline function
         """
-        super().__init__(env)                                                                                       # Store the environment object
-        self.__episodes = episodes                                                                                  # Store the number of training episodes
-        self.__discount = discount                                                                                  # Store the discount factor
-        self.__step_size_theta = step_size_theta                                                                    # Store the parameter stepsize
-        self.__flags = flags                                                                                        # Store the state of the flags
-        if flags is None:                                                                                           # If no flag argument used...
-            self.__flags = [0]                                                                                      # Initialise a flag array with default flags
-        elif flags[0]:                                                                                              # If the first flag is true...
-            self.__step_size_w = step_size_w                                                                        # Store the baseline function stepsize
-            self.__value_network = ValueNet(np.prod(env.observation_space.shape))                                   # Store the value estimation neural network
-        self.__discrete_actions = (type(env.action_space) == spaces.Discrete)                                       # Determine the type of action space
-        if self.__discrete_actions:                                                                                 # If the action space is discrete...
-            self.__policy_network = DiscretePNN(np.prod(env.observation_space.shape), env.action_space.n)           # Create and store a policy NN for discrete action space
-        else:                                                                                                       # If the action space is continuous...
-            self.__policy_network = ContinuousPNN(np.prod(env.observation_space.shape), env.action_space.shape[0])  # Create and store a policy NN for continuous action space
+        super().__init__(env)                                                                                           # Store the environment object
+        self.__episodes = episodes                                                                                      # Store the number of training episodes
+        self.__discount = discount                                                                                      # Store the discount factor
+        self.__step_size_theta = step_size_theta                                                                        # Store the parameter stepsize
+        self.__flags = flags                                                                                            # Store the state of the flags
+        if flags is None:                                                                                               # If no flag argument used...
+            self.__flags = [0]                                                                                          # Initialise a flag array with default flags
+        elif flags[0]:                                                                                                  # If the first flag is true...
+            self.__step_size_w = step_size_w                                                                            # Store the baseline function stepsize
+            self.__value_network = ValueNet(np.prod(env.observation_space.shape))                                       # Store the value estimation neural network
+        self.__discrete_actions = (type(env.action_space) == spaces.Discrete)                                           # Determine the type of action space
+        if self.__discrete_actions:                                                                                     # If the action space is discrete...
+            self.__policy_network = DiscretePNN(np.prod(env.observation_space.shape), env.action_space.n)               # Create and store a policy NN for discrete action space
+        else:                                                                                                           # If the action space is continuous...
+            self.__policy_network = ContinuousPNN(np.prod(env.observation_space.shape), env.action_space.shape[0])      # Create and store a policy NN for continuous action space
 
     def __generate_trajectory(self):
-        tau, done, truncated = [], False, False                             # Initialise empty trajectory and exit flags
-        obs, info = self.env.reset()                                        # Get current state
-        while not (done or truncated):                                      # Until a terminal state is reached
-            action = self.predict(obs)                                      # Get next action based on agent's policy
-            new_obs, reward, done, truncated, info = self.env.step(action)  # Take action and observe environment
-            tau += [[action, obs, reward]]                                  # Add observation to trajectory
-            obs = new_obs                                                   # Update state
-        return tau                                                          # Return trajectory
+        tau, done, truncated = [], False, False                                                                         # Initialise empty trajectory and exit flags
+        obs, info = self.env.reset()                                                                                    # Get current state
+        while not (done or truncated):                                                                                  # Until a terminal state is reached
+            action = self.predict(obs)                                                                                  # Get next action based on agent's policy
+            new_obs, reward, done, truncated, info = self.env.step(action)                                              # Take action and observe environment
+            tau += [[action, obs, reward]]                                                                              # Add observation to trajectory
+            obs = new_obs                                                                                               # Update state
+        return tau                                                                                                      # Return trajectory
 
     def __estimate_value(self, state):
-        tensor = torch.tensor(state.flatten(), dtype=torch.float32) # Convert the state into a float tensor
-        return self.__value_network(tensor).detach()                # Return the estimated state value (detached from the NN)
+        tensor = torch.tensor(state.flatten(), dtype=torch.float32)                                                     # Convert the state into a float tensor
+        return self.__value_network(tensor).detach()                                                                    # Return the estimated state value (detached from the NN)
 
     def __update_parameters(self, d, t=None):
-        network = self.__value_network if t is None else self.__policy_network              # Determine which network to update
-        network.zero_grad()                                                                 # Zero the gradients before the backwards pass
-        network.backward()                                                                  # TODO fix
-        with torch.no_grad():                                                               # Don't track parameter updates (prevents auto grad errors)
-            for param in network.parameters():                                              # Loop over and update the network parameters
-                #print(f"update = {self.__step_size_theta} * ({self.__discount} ** {t}) * {d} * {param.grad}")
-                param += self.__step_size_theta * (self.__discount ** t) * d * param.grad   # Update the network parameters (gradient ascent) # TODO fix param.grad Dlog(policy(A_t | S_t, Theta))), Dv(s_t,w)
+        network = self.__value_network if t is None else self.__policy_network                                          # Determine which network to update
+        network.zero_grad()                                                                                             # Zero the gradients before the backwards pass
+        network.backward()                                                                                              # TODO fix
+        with torch.no_grad():                                                                                           # Don't track parameter updates (prevents auto grad errors)
+            for param in network.parameters():                                                                          # Loop over and update the network parameters
+                param += self.__step_size_theta * (self.__discount ** t) * d * param.grad                               # Update the network parameters (gradient ascent) # TODO fix param.grad Dlog(policy(A_t | S_t, Theta))), Dv(s_t,w)
 
     def predict(self, obs):
-        tensor = torch.tensor(obs.flatten(), dtype=torch.float32)   # Convert the state into a float tensor
-        if self.__discrete_actions:                                 # If discrete action space...
-            action = self.__policy_network(tensor)                  # Compute the action from the policy net
-            action = torch.argmax(action)                           # Select index of highest value action (greedy) # TODO sample
-        else:                                                       # If continuous action space...
-            means, stds = self.__policy_network(tensor)             # Compute the mean and standard deviation of each type of action
-            action = means                                          # Select means of each type of action (greedy) # TODO sample
-        return action                                               # Return the policy's chosen action
+        tensor = torch.tensor(obs.flatten(), dtype=torch.float32)                                                       # Convert the state into a float tensor
+        if self.__discrete_actions:                                                                                     # If discrete action space...
+            action = self.__policy_network(tensor)                                                                      # Compute the action from the policy net
+            action = torch.argmax(action)                                                                               # Select index of highest value action (greedy) # TODO sample
+        else:                                                                                                           # If continuous action space...
+            means, stds = self.__policy_network(tensor)                                                                 # Compute the mean and standard deviation of each type of action
+            action = means                                                                                              # Select means of each type of action (greedy) # TODO sample
+        return action                                                                                                   # Return the policy's chosen action
 
     def learn(self):
-        for episode in range(self.__episodes):                                                          # Train using self.__episodes number of trajectories
-            trajectory = self.__generate_trajectory()                                                   # Generate a trajectory following the agent's current policy
-            for step_t in range(0, len(trajectory)):                                                    # Loop t from t=0 to t=T where T is the number of timesteps in the trajectory
-                reward_to_go = 0                                                                        # Initialise 'G' to 0
-                for step_k in range(step_t + 1, len(trajectory) + 1):                                   # Loop through each SAR of the sub-trajectory
-                    reward_to_go += (self.__discount ** (step_k-step_t-1)) * trajectory[step_k-1][2]    # add to the running sum, G
-                delta = reward_to_go                                                                    # Initialise new variable in case of baseline function
-                if self.__flags[0]:                                                                     # "If including baseline..."
-                    delta -= self.__estimate_value(trajectory[step_t][1])                               # delta = G - v(s_t, w)
-                    self.__update_parameters(delta)                                                     # w = w + (alpha_w * delta * Dv(s_t,w))
-                self.__update_parameters(delta, step_t)                                                 # theta = theta + (alpha_theta * (discount ** t) * delta * Dlog(policy(A_t | S_t, Theta)))
+        for episode in range(self.__episodes):                                                                          # Train using self.__episodes number of trajectories
+            trajectory = self.__generate_trajectory()                                                                   # Generate a trajectory following the agent's current policy
+            for step_t in range(0, len(trajectory)):                                                                    # Loop t from t=0 to t=T where T is the number of timesteps in the trajectory
+                reward_to_go = 0                                                                                        # Initialise 'G' to 0
+                for step_k in range(step_t + 1, len(trajectory) + 1):                                                   # Loop through each SAR of the sub-trajectory
+                    reward_to_go += (self.__discount ** (step_k-step_t-1)) * trajectory[step_k-1][2]                    # add to the running sum, G
+                delta = reward_to_go                                                                                    # Initialise new variable in case of baseline function
+                if self.__flags[0]:                                                                                     # "If including baseline..."
+                    delta -= self.__estimate_value(trajectory[step_t][1])                                               # delta = G - v(s_t, w)
+                    self.__update_parameters(delta)                                                                     # w = w + (alpha_w * delta * Dv(s_t,w))
+                self.__update_parameters(delta, step_t)                                                                 # theta = theta + (alpha_theta * (discount ** t) * delta * Dlog(policy(A_t | S_t, Theta)))
