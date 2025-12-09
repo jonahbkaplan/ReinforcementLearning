9be775305f7d6568cfcded0808dc85a8abc9ec41
rj-carey <carey.ruaidhri@gmail.com>
2025-12-09 13:03:47 +0000

    Working(?) discrete w/o baseline.

--- algorithms/REINFORCE.py
+++ algorithms/REINFORCE.py
@@ -8,7 +8,7 @@ import torch
 
 
 class Reinforce(Agent):
-    def __init__(self, env, episodes=100, discount=0.9, step_size_theta=2e-9, step_size_w=2e-6, flags=None):
+    def __init__(self, env, episodes=100, discount=0.9, step_size_theta=2**-13, step_size_w=2e-5, flags=None):
         """
         REINFORCE with optional baseline function.
 
@@ -61,32 +61,112 @@ class Reinforce(Agent):
         return action                                                                                                   # Return the policy's chosen action
 
     def learn(self, verbose=False):
-        undiscounted_rewards, average_rewards = [], []                                                                  # Initialise storage for evaluation metrics
-        for episode in range(self.__episodes):                                                                          # Train using self.__episodes number of trajectories
-            states_tensor, actions_tensor, rewards_tensor = self.__generate_trajectory()                                # Generate a trajectory following the agent's current policy
-            timesteps = rewards_tensor.size(dim=0)                                                                       # Store the length of this episode's trajectory
-            discounts_tensor = torch.tensor([self.__discount**t for t in range(timesteps)], dtype=torch.float32)        # Generate discounts tensor
-            discounted_rewards = [discounts_tensor[:timesteps-t] * rewards_tensor[t:] for t in range(timesteps)]        # Apply discounts to returns
-            r_t_g_tensor = torch.tensor([torch.sum(discounted_rewards[t]) for t in range(timesteps)], dtype=torch.float32) # Compute rewards to go
-            if self.__flags[0]:                                                                                         # If including baseline...
-                values = self.__value_network(states_tensor)                                                            # Collect V(s_t, w) for all t
-                r_t_g_tensor = r_t_g_tensor - values.detach()                                                           # delta = G - v(s_t, w)
-                w_loss = torch.sum(values * r_t_g_tensor)                                                               # Apply the weights: (alpha_w * (discount ** t) * delta) and sum the losses for all timesteps
-                self.__value_optimiser.zero_grad()                                                                      # Clear the gradients
-                w_loss.backward()                                                                                       # Compute gradients
-                self.__value_optimiser.step()                                                                           # Gradient ascent: w += (alpha_w * (discount ** t) * delta * Dv(s_t,w))
-            action_probabilities = self.__policy_network(states_tensor)                                                 # Compute the action probabilities for each state
-            if self.__discrete_actions:                                                                                 # If the environment uses a discrete action space...
-                distributions = torch.distributions.Categorical(action_probabilities)                                   # Create categorical distributions from action probabilities
-            else:                                                                                                       # If continuous action space...
-                distributions = None # TODO continuous action space
-            log_probabilities = - distributions.log_prob(actions_tensor)                                                # Compute the log of the probability of the actions taken (minus for gradient ascent)
-            theta_loss = torch.sum(log_probabilities * r_t_g_tensor)                                                    # Apply the weights: (alpha_theta * (discount ** t) * delta) and sum the losses for all timesteps
-            self.__policy_optimiser.zero_grad()                                                                         # Clear the gradients
-            theta_loss.backward()                                                                                       # Compute gradients
-            self.__policy_optimiser.step()                                                                              # Gradient ascent: theta += (alpha_theta * (discount ** t) * delta * Dlog(policy(A_t | S_t, Theta)))
-            undiscounted_rewards.append(torch.sum(rewards_tensor))                                                      # Append episode metrics
-            average_rewards.append(torch.sum(rewards_tensor)/timesteps)                                                 # Append episode metrics
-            if verbose:                                                                                                 # If verbose...
-                print(f"Episode: {episode + 1}, Reward: {undiscounted_rewards[episode]:.2f}")                           # Update user of progress
-        return undiscounted_rewards, average_rewards                                                                    # Return metrics for evaluation
+        # Metrics
+        undiscounted_rewards, average_rewards = [], []
+
+        # Training
+        for episode in range(self.__episodes):
+            # Generate a sample trajectory using the current policy
+            states_tensor, actions_tensor, rewards_tensor = self.__generate_trajectory()
+            # Store the length of this episode's trajectory
+            timesteps = rewards_tensor.size(dim=0)
+
+            # Step through the trajectory
+            for t in range(timesteps):
+                ## ACCURACY AT COST OF SPEED:
+                # Compute reward-to-go
+                discounts = self.__discount ** torch.arange(timesteps - t)
+                r_t_g = torch.sum(discounts * rewards_tensor[t:])
+
+                # If using baseline, update r_t_g to delta and update w parameters
+                if self.__flags[0]:
+                    pass # TODO once without baseline stabilised
+
+                # Updates theta parameters
+                action_probabilities = self.__policy_network(states_tensor[t])
+                # Get log probability of action take
+                if self.__discrete_actions:
+                    dist = torch.distributions.Categorical(action_probabilities)
+                else:
+                    pass # TODO once discrete action stabilised
+                log_probability = - dist.log_prob(actions_tensor[t])
+
+                # Apply loss for gradient ascent
+                theta_loss = log_probability * r_t_g
+
+                # Complete the parameter update
+                self.__policy_optimiser.zero_grad()
+                theta_loss.backward()
+                self.__policy_optimiser.step()
+
+            # Append new metrics
+            undiscounted_rewards.append(torch.sum(rewards_tensor))
+            average_rewards.append(torch.sum(rewards_tensor) / timesteps)
+
+            # Report to user
+            if verbose:
+                print(f"Episode: {episode + 1}, Reward: {undiscounted_rewards[episode]:.2f}")
+
+        # Return metrics for evaluation
+        return undiscounted_rewards, average_rewards
+
+            #discounts_tensor = torch.tensor([self.__discount**t for t in range(timesteps)], dtype=torch.float32)        # Generate discounts tensor
+            #discounted_rewards = [discounts_tensor[:timesteps-t] * rewards_tensor[t:] for t in range(timesteps)]        # Apply discounts to returns
+            #r_t_g_tensor = torch.tensor([torch.sum(discounted_rewards[t]) for t in range(timesteps)], dtype=torch.float32) # Compute rewards to go
+            #if self.__flags[0]:                                                                                         # If including baseline...
+                #values = self.__value_network(states_tensor)                                                            # Collect V(s_t, w) for all t
+                #r_t_g_tensor = r_t_g_tensor - values.detach()                                                           # delta = G - v(s_t, w)
+                #w_loss = torch.sum(values * r_t_g_tensor)                                                               # Apply the weights: (alpha_w * (discount ** t) * delta) and sum the losses for all timesteps
+                #self.__value_optimiser.zero_grad()                                                                      # Clear the gradients
+                #w_loss.backward()                                                                                       # Compute gradients
+                #self.__value_optimiser.step()                                                                           # Gradient ascent: w += (alpha_w * (discount ** t) * delta * Dv(s_t,w))
+            #action_probabilities = self.__policy_network(states_tensor)                                                 # Compute the action probabilities for each state
+
+if __name__ == '__main__':
+    import gymnasium as gym
+    import highway_env
+    from matplotlib import pyplot as plt
+
+    # Controls:
+    CONTINUOUS_ACTIONS = False
+    TESTING = True
+    EPISODES = 1000
+    ###########
+
+    if CONTINUOUS_ACTIONS and TESTING:
+        env = gym.make("Pendulum-v1", render_mode="rgb_array")
+    elif CONTINUOUS_ACTIONS and not TESTING:
+        env = gym.make('highway-fast-v0', config={"action": {"type": "ContinuousAction"}}, render_mode='rgb_array')
+    elif not CONTINUOUS_ACTIONS and TESTING:
+        env = gym.make("CartPole-v1", render_mode="rgb_array")
+    elif not CONTINUOUS_ACTIONS and not TESTING:
+        env = gym.make('highway-fast-v0', render_mode='rgb_array')
+    else:
+        print("invalid controls, aborting...")
+        quit()
+
+    model = Reinforce(env, episodes=EPISODES, flags=[0])
+    rs, ars = model.learn(verbose=True)
+
+    #done = truncated = False
+    #obs, info = env.reset()
+    #while not (done or truncated):
+    #    action = model.predict(obs)
+    #    obs, reward, done, truncated, info = env.step(action)
+    #    env.render()
+
+    #plt.imshow(env.render())
+    #plt.show()
+    #env.close()
+
+    plt.plot(rs)
+    plt.title("Undiscounted Total Rewards vs Episodes")
+    plt.xlabel("Episode")
+    plt.ylabel("Undiscounted Total Reward")
+
+    #axes[1].plot(ars)
+    #axes[1].set_title("Average Reward vs Episodes")
+    #axes[1].set_xlabel("Episode")
+    #axes[1].set_ylabel("Average Reward")
+
+    plt.show()
