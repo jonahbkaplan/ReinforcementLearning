6d14430866dd30586571771f617c22d47f9a136e
rj-carey <carey.ruaidhri@gmail.com>
2025-12-09 16:36:51 +0000

    Progress on continuous (working through errors).

--- algorithms/REINFORCE.py
+++ algorithms/REINFORCE.py
@@ -19,46 +19,103 @@ class Reinforce(Agent):
         :param Real step_size_w: Step size for baseline function updates (Alpha_w)
         :param Array flags: index 0 indicates true or false to include baseline function
         """
-        super().__init__(env)                                                                                           # Store the environment object
-        self.__episodes = episodes                                                                                      # Store the number of training episodes
-        self.__discount = discount                                                                                      # Store the discount factor
-        self.__flags = flags                                                                                            # Store the state of the flags
-        if flags is None:                                                                                               # If no flag argument used...
-            self.__flags = [0]                                                                                          # Initialise a flag array with default flags
-        elif flags[0]:                                                                                                  # If the first flag is true...
-            self.__value_network = ValueNet(np.prod(env.observation_space.shape))                                       # Store the value estimation neural network
-            self.__value_optimiser = torch.optim.SGD(self.__value_network.parameters(), lr=step_size_w)                 # Initialise baseline function optimiser
-        self.__discrete_actions = (type(env.action_space) == spaces.Discrete)                                           # Determine the type of action space
-        if self.__discrete_actions:                                                                                     # If the action space is discrete...
-            self.__policy_network = DiscretePNN(np.prod(env.observation_space.shape), env.action_space.n)               # Create and store a policy NN for discrete action space
-        else:                                                                                                           # If the action space is continuous...
-            self.__policy_network = ContinuousPNN(np.prod(env.observation_space.shape), env.action_space.shape[0])      # Create and store a policy NN for continuous action space
-        self.__policy_optimiser = torch.optim.SGD(self.__policy_network.parameters(), lr=step_size_theta)               # Initialise policy estimation optimiser
+        # Store parameters required for training and predicting
+        super().__init__(env)
+        self.__episodes = episodes
+        self.__discount = discount
+        self.__flags = flags
+
+        # If no flags given (currently only for baseline function)
+        if flags is None:
+            # Initialise array with default flags
+            self.__flags = [0]
+        elif flags[0]:
+            # If using baseline function, initialise value approximator and optimiser
+            self.__value_network = ValueNet(np.prod(env.observation_space.shape))
+            self.__value_optimiser = torch.optim.SGD(self.__value_network.parameters(), lr=step_size_w)
+
+        # Determine the type of action space
+        self.__discrete_actions = (type(env.action_space) == spaces.Discrete)
+        if self.__discrete_actions:
+            # Create and store a policy NN for discrete action space
+            self.__policy_network = DiscretePNN(np.prod(env.observation_space.shape), env.action_space.n)
+        else:
+            # Create and store a policy NN for continuous action space
+            self.__policy_network = ContinuousPNN(np.prod(env.observation_space.shape), env.action_space.shape[0])
+            # Initialise storage for raw action
+            self.__raw_action = None
+            # Determine and store the bounds of each action dimension
+            self.__lower_bounds, self.__upper_bounds = torch.tensor(env.action_space.low), torch.tensor(env.action_space.high)
+        # Initialise policy estimation optimiser
+        self.__policy_optimiser = torch.optim.SGD(self.__policy_network.parameters(), lr=step_size_theta)
 
     def __generate_trajectory(self):
-        states, actions, rewards, done, truncated = [], [], [], False, False                                            # Initialise empty trajectories and exit flags
-        state, info = self.env.reset()                                                                                  # Get current state
-        while not (done or truncated):                                                                                  # Until a terminal state is reached
-            action = self.predict(state)                                                                                # Get next action based on agent's policy
-            new_state, reward, done, truncated, info = self.env.step(action)                                            # Take action and observe environment
-            states.append(torch.tensor(state.flatten(), dtype=torch.float32))                                           # Add state to trajectory
-            actions.append(action)                                                                                      # Add action to trajectory
-            rewards.append(reward)                                                                                      # Add reward to trajectory
-            state = new_state                                                                                           # Update state
-        return torch.stack(states), torch.tensor(actions), torch.tensor(rewards)                                        # Return trajectory
+        # Initialise empty trajectories and exit flags
+        states, actions, raw_actions, rewards, done, truncated = [], [], [], [], False, False
+        # Get current state
+        state, info = self.env.reset()
 
-    def predict(self, obs):
-        tensor = torch.tensor(obs.flatten(), dtype=torch.float32)                                                       # Convert the state into a float tensor
-        if self.__discrete_actions:                                                                                     # If discrete action space...
-            action = self.__policy_network(tensor)                                                                      # Compute the action from the policy net
-            action = torch.distributions.Categorical(action).sample().item()                                            # Select index of highest value action
-        else:                                                                                                           # If continuous action space...
-            means, log_stds = self.__policy_network(tensor)                                                             # Compute the mean and log standard deviation of each type of action
-            stds = log_stds.exp()                                                                                       # Convert log stds to regular stds
-            dists = torch.distributions.Normal(means, stds)                                                             # Convert distribution parameters to distributions
-            dists = torch.distributions.Independent(dists, 1)                                      # Treat action dimensions jointly
-            action = dists.sample()                                                                                     # Take samples of each action dimension from the distributions
-        return action                                                                                                   # Return the policy's chosen action
+        # Until a terminal state is reached
+        while not (done or truncated):
+            # Get next action based on agent's policy, with chance of exploration
+            action = self.predict(state, greedy=False)
+
+            # If continuous action space, get raw action and append to storage
+            if not self.__discrete_actions:
+                raw_actions.append(self.__raw_action)
+
+            # Take action and observe environment
+            new_state, reward, done, truncated, info = self.env.step(action)
+
+            # Add state, action, and reward to trajectories
+            states.append(torch.tensor(state.flatten(), dtype=torch.float32))
+            actions.append(action)
+            rewards.append(reward)
+
+            # Update state
+            state = new_state
+        # Return trajectory
+        return torch.stack(states), torch.tensor(actions), torch.tensor(raw_actions), torch.tensor(rewards)
+
+    def predict(self, obs, greedy=True):
+        # Convert the state into a float tensor
+        tensor = torch.tensor(obs.flatten(), dtype=torch.float32)
+
+        # If discrete action space...
+        if self.__discrete_actions:
+            # Compute action probabilities
+            action = self.__policy_network(tensor)
+            # Return best action if greedy
+            if greedy:
+                return torch.argmax(action).item()
+            else:
+                # Otherwise return a sample from the distribution
+                return torch.distributions.Categorical(action).sample().item()
+
+        # If continuous action space...
+        else:
+            # Compute the mean and log standard deviation of each type of action
+            means, log_stds = self.__policy_network(tensor)
+            # Convert log stds to regular stds
+            stds = log_stds.exp()
+            # Return best actions if greedy
+            if greedy:
+                actions = means
+            else:
+                # Otherwise return samples from the distribution
+                dists = torch.distributions.Normal(means, stds)
+                # Treat action dimensions jointly
+                dists = torch.distributions.Independent(dists, 1)
+                # Sample action
+                actions = dists.rsample()
+            # Store raw action for gradient ascent
+            self.__raw_action = actions
+            # Scale actions to [-1,1]
+            actions = torch.tanh(actions)
+            # Stretch actions to action space (from [-1,1] to [lower,upper])
+            actions = self.__lower_bounds + (actions+1)*0.5*(self.__upper_bounds-self.__lower_bounds)
+            # Take samples of each action dimension from the distributions
+            return actions.detach()
 
     def learn(self, verbose=False):
         # Metrics
@@ -67,13 +124,12 @@ class Reinforce(Agent):
         # Training
         for episode in range(self.__episodes):
             # Generate a sample trajectory using the current policy
-            states_tensor, actions_tensor, rewards_tensor = self.__generate_trajectory()
+            states_tensor, actions_tensor, raw_actions_tensor, rewards_tensor = self.__generate_trajectory()
             # Store the length of this episode's trajectory
             timesteps = rewards_tensor.size(dim=0)
 
             # Step through the trajectory
             for t in range(timesteps):
-                ## ACCURACY AT COST OF SPEED:
                 # Compute reward-to-go
                 discounts = self.__discount ** torch.arange(timesteps - t)
                 r_t_g = torch.sum(discounts * rewards_tensor[t:])
@@ -92,20 +148,26 @@ class Reinforce(Agent):
                     self.__value_optimiser.step()
 
                 # Updates theta parameters
-                action_probabilities = self.__policy_network(states_tensor[t])
                 # Get log probability of action take
                 if self.__discrete_actions:
+                    action_probabilities = self.__policy_network(states_tensor[t])
                     dist = torch.distributions.Categorical(action_probabilities)
+                    log_probability = dist.log_prob(actions_tensor[t])
                 else:
-                    pass # TODO once discrete action stabilised
-                log_probability = - dist.log_prob(actions_tensor[t])
+                    means, log_stds = self.__policy_network(states_tensor[t])
+                    stds = log_stds.exp()
+                    dist = torch.distributions.Normal(means, stds)
+                    dist = torch.distributions.Independent(dist, 1)
+
+                    # Jacobian correction required due to tanh squashing (non-linear transformation)
+                    log_probability = (dist.log_prob(raw_actions_tensor[t]) - torch.log(1 - torch.tanh(raw_actions_tensor[t]).pow(2) + 1e-6)).sum(-1)
 
                 # Apply loss for gradient ascent
-                theta_loss = log_probability * r_t_g
+                theta_loss = -log_probability * r_t_g
 
                 # Complete the parameter update
                 self.__policy_optimiser.zero_grad()
-                theta_loss.backward()
+                theta_loss.backward() # TODO fix
                 self.__policy_optimiser.step()
 
             # Append new metrics
@@ -126,9 +188,9 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
 
     # Controls:
-    CONTINUOUS_ACTIONS = False
+    CONTINUOUS_ACTIONS = True
     TESTING = True
-    EPISODES = 1000
+    EPISODES = 1
     BASELINE = True
     ###########
 
